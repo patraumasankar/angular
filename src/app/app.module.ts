79d2db45242a688719f1d725aa49bc49d5c07427
Umasankar Patra <patraumasankar18@gmail.com>
2022-06-22 13:21:23 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {Component} from '@angular/core';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  
+ }
